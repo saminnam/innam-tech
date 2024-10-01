b7993d4ebb8dfd6d768144ee3f4052ffc8307dbe
saminnam <innam8264@gmail.com>
2024-10-01 15:35:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import './App.css';
-import 'aos/dist/aos.css';
-import AOS from 'aos';
-import { useEffect } from 'react';
+import "./App.css";
+import "aos/dist/aos.css";
+import AOS from "aos";
+import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
     // Initialize AOS with custom settings for smoother animations
     AOS.init({
-      duration: 1000,  // Controls the duration of the animations (in ms)
-      easing: 'ease-in-out',  // Easing function for smoother transitions
-      once: true,  // Whether animation should happen only once or every time you scroll
-      delay: 100,  // Adds a delay before animations start
+      duration: 1000, // Controls the duration of the animations (in ms)
+      easing: "ease-in-out", // Easing function for smoother transitions
+      once: true, // Whether animation should happen only once or every time you scroll
+      delay: 100, // Adds a delay before animations start
     });
-  },[])
+  }, []);
   AOS.init();
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
